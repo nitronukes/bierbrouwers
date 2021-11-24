58b408775f4a4fdbfa7a614f8d10178a208b85b6
Shibin0613 <220617@edu.rocfriesepoort.nl>
2021-11-24 09:59:50 +0100

    Update bestel(particulier).php

--- bestel(particulier).php
+++ bestel(particulier).php
@@ -111,5 +111,6 @@ var month= date.getMonth()+1;
 var todayDate = String(date.getDate()).padStart(2,'0');
 var datePattern = year + '-' + month + '-' + todayDate;
 document.getElementById("datePickerId").value = datePattern;
+
 </script>
 </html>
