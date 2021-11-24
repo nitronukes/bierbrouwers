8d5fe8286421dc9c66b3067b6e6fd24582072f7a
Shibin0613 <220617@edu.rocfriesepoort.nl>
2021-11-24 08:39:29 +0100

    Update bestel(zakelijk)1.php

--- bestel(zakelijk)1.php
+++ bestel(zakelijk)1.php
@@ -1,3 +1,11 @@
+<?php
+include("conn.php");
+error_reporting(0);
+$query= "select * from adminaccountmanage";
+$data = mysqli_query($conn,$query);
+$total = mysqli_num_rows($data);
+?>
+
 <!DOCTYPE html>
 <html lang="nl">
 <head>
@@ -7,38 +15,36 @@
   <link rel="stylesheet" href="main.css">
 </head>
 
-<body>
 <header>
     <div class="login-button">
-      <a href="mainphp.php"> <button type="submit">login</button></a>
-</div>
-  </header>
-
-</body>
-
-
-<?php
-include("conn.php");
-error_reporting(0);
-$query= "select * from adminaccountmanage";
-$data = mysqli_query($conn,$query);
-$total = mysqli_num_rows($data);
-
-if($total!=0){
-    while($result=mysqli_fetch_assoc($data)){
-        echo "
-        <a href='update(zakelijk).php?bedrijfsnaam=$result[bedrijfsnaam]&naam=$result[naam]&telef=$result[telef]&email=$result[email]&bezorgadres=$result[bezorgadres]&bpostcode=$result[bpostcode]&username=$result[username]'>account</a>
-        ";
-    }
-  }
+      <a href="mainphp.php"> <button type="submit">loguit</button></a>
+   </div>
+     <div class="NAV">
+      <nav>
+        <ul>
+          <li>
+          <?php
+          if($total!=0){
+              echo "
+              <a href='update(zakelijk).php?username=$_SESSION[username]&naam=$result[naam]&telef=$result[telef]&email=$result[email]&bezorgadres=$result[bezorgadres]&bpostcode=$result[bpostcode]&username=$result[username]'>account</a>
+              ";
+            }
 ?>
+        </li>
+        </ul>
+      </nav>
+    </div>
+</header>
+
+
+
 
 <div class="container1">
     <h1>appel Bandit</h1>
   <img src="https://media.foox.nl/FT/AFB/high/35033.jpg" id="besimg">
 
   <form method="GET"><p id="pp"><input type="number" name="aantal" placeholder="aantal" min="1" max="99999999" required></P>
-  <button type="submit" class="sub" value="Aantal Bevestigen">Aantal Bevestigen</button>
+  <button type="submit" class="sub">Aantal Bevestigen</button>
       
 </div>
 <div class="table">
@@ -79,7 +85,7 @@ if($total!=0){
     
     
     <div class="container3">
-    <button type="submit" class="sub" value="Bestel">BESTEL</button>
+    <button type="submit" class="sub">BESTEL</button>
     </div>
 
 <script>
